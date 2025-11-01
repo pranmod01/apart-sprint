09d9669ded7516a7d05bb4b0b85f7bbde6ce45ee
Kalpesh Panchal <panchalkalpesh@gmail.com>
2025-11-01 13:48:54 -0400

    feat: add Cube component for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Cube from './components/Cube'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/* <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Cube position={[-1.2, 0, 0]} />
+        <Cube position={[1.2, 0, 0]} />
+      </Canvas> */}
       <section className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
