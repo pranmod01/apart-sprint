6fb46f9b863ebb2582a8fb60315bd856b0010e06
Kalpesh Panchal <panchalkalpesh@gmail.com>
2025-11-02 12:57:37 -0500

    feat: introduce COLORS config for 3D Visualization

---
+++ src/config/colors.ts
@@ -0,0 +1,12 @@
+/**
+ * Colors to be used within 3D Visualization
+ */
+export const COLORS = {
+  deepSinkhole: "#1a0f0f",
+  sinkhole: "#471515",
+  lowland: "#234d1f",
+  grassland: "#526f35",
+  highland: "#8b7355",
+  mountain: "#9c917f",
+  peak: "#e8e8e8",
+};
